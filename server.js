715af6d20c6677bdff3537f397fd53c20bfd4da4
mrparveen99927 <mr.parveen99927@gmail.com>
2026-06-28 22:22:58 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
-// सभी राउट्स इम्पोर्ट करना
 const userRoutes = require('./routes/userRoutes');
 const walletRoutes = require('./routes/walletRoutes');
 const adminRoutes = require('./routes/adminRoutes');
@@ -13,7 +12,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// मुख्य रूट चेक
 app.get('/', (req, res) => {
     res.json({ 
         message: "New Central Wallet V2 Engine is 100% Live & Functional!",
@@ -22,12 +20,10 @@ app.get('/', (req, res) => {
     });
 });
 
-// राउट्स लिंक करना
 app.use('/api/users', userRoutes);
 app.use('/api/wallet', walletRoutes);
 app.use('/api/admin', adminRoutes);
 
-// MongoDB Atlas कनेक्शन
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("✓ MongoDB Atlas Connected Successfully to Central Wallet DB!"))
 .catch((err) => console.error("✗ MongoDB Connection Error:", err));
